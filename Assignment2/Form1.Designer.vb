d4e444a8f15bfee48ddacced67f0c0fc8aa3a7d3
sidjethwani@gmail.com <sidjethwani@gmail.com>
2024-02-17 05:01:49 +0530

    Abacus Basic Version 1

--- Assignment2/Form1.Designer.vb
+++ Assignment2/Form1.Designer.vb
@@ -22,9 +22,54 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        components = New System.ComponentModel.Container()
+        Me.ShapeContainer1 = New Microsoft.VisualBasic.PowerPacks.ShapeContainer()
+        Me.LineShape1 = New Microsoft.VisualBasic.PowerPacks.LineShape()
+        Me.OvalShape1 = New Microsoft.VisualBasic.PowerPacks.OvalShape()
+        Me.SuspendLayout()
+        '
+        'ShapeContainer1
+        '
+        Me.ShapeContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ShapeContainer1.Margin = New System.Windows.Forms.Padding(0)
+        Me.ShapeContainer1.Name = "ShapeContainer1"
+        Me.ShapeContainer1.Shapes.AddRange(New Microsoft.VisualBasic.PowerPacks.Shape() {Me.LineShape1, Me.OvalShape1})
+        Me.ShapeContainer1.Size = New System.Drawing.Size(757, 1055)
+        Me.ShapeContainer1.TabIndex = 0
+        Me.ShapeContainer1.TabStop = False
+        '
+        'LineShape1
+        '
+        Me.LineShape1.Name = "LineShape1"
+        Me.LineShape1.Visible = False
+        Me.LineShape1.X1 = 323
+        Me.LineShape1.X2 = 398
+        Me.LineShape1.Y1 = 141
+        Me.LineShape1.Y2 = 164
+        '
+        'OvalShape1
+        '
+        Me.OvalShape1.Location = New System.Drawing.Point(157, 122)
+        Me.OvalShape1.Name = "OvalShape1"
+        Me.OvalShape1.Size = New System.Drawing.Size(75, 23)
+        Me.OvalShape1.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.FromArgb(CType(CType(192, Byte), Integer), CType(CType(255, Byte), Integer), CType(CType(192, Byte), Integer))
+        Me.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom
+        Me.ClientSize = New System.Drawing.Size(757, 1055)
+        Me.Controls.Add(Me.ShapeContainer1)
+        Me.ForeColor = System.Drawing.SystemColors.ControlLightLight
+        Me.Margin = New System.Windows.Forms.Padding(4)
+        Me.Name = "Form1"
         Me.Text = "Form1"
+        Me.ResumeLayout(False)
+
     End Sub
+    Friend WithEvents ShapeContainer1 As Microsoft.VisualBasic.PowerPacks.ShapeContainer
+    Friend WithEvents LineShape1 As Microsoft.VisualBasic.PowerPacks.LineShape
+    Friend WithEvents OvalShape1 As Microsoft.VisualBasic.PowerPacks.OvalShape
 
 End Class
--- Assignment2/Form1.vb
+++ Assignment2/Form1.vb
@@ -1,3 +1,58 @@
 ﻿Public Class Form1
 
+    Dim Abacus_input1(8, 5) As Abacus_Bead
+    Dim Abacus_input2(8, 5) As Abacus_Bead
+    Dim Abacus_result(8, 5) As Abacus_Bead
+
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Me.SuspendLayout()
+
+        Dim locx As Integer = 30
+        Dim locy As Integer = 100
+
+        For i As Integer = 0 To 7
+            For j As Integer = 0 To 4
+                Abacus_input1(i, j) = New Abacus_Bead()
+
+                Abacus_input1(i, j).Name = String.Format("{0}-{1}-{2}", 1, i, j)
+                Abacus_input1(i, j).Location = New System.Drawing.Point(locx, locy)
+                Abacus_input1(i, j).Size = New System.Drawing.Size(25, 10)
+
+                Me.ShapeContainer1.Shapes.Add(Abacus_input1(i, j))
+
+
+                locy += 10
+            Next
+            locy = 100
+            locx += 30
+        Next
+
+        Me.ResumeLayout(False)
+    End Sub
+
+    Private Sub BeadHandler(ByVal sender As System.Object, ByVal e As System.EventArgs)
+        Dim clicked_bead As Abacus_Bead = DirectCast(sender, Abacus_Bead)
+
+
+    End Sub
+End Class
+
+Public Class Abacus_Bead
+    Inherits Microsoft.VisualBasic.PowerPacks.OvalShape
+
+    Private _state As Integer
+    Private Active As System.Drawing.Point
+    Private DeActive As System.Drawing.Point
+
+    Public Property State As Integer
+        Get
+            Return _state
+        End Get
+        Set(ByVal value As Integer)
+            _state = value
+        End Set
+    End Property
+
+
 End Class
